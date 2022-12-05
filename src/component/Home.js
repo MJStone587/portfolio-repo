b853724c54fd722a50f1d696a74760df75cd21c4
MJStone587 <MJstone587@yahoo.com>
2022-12-05 12:24:18 -0600

    working a bit with framer motion and other animation methods to create a bit more of a unique experience

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,14 +1,24 @@
 import React, { forwardRef, useRef } from "react";
 import Typewriter from "typewriter-effect";
 import "animate.css";
+import { useScroll, motion, AnimatePresence } from "framer-motion";
 
 const Home = (props, ref) => {
   const aboutRef = useRef();
+  const animRef = useRef(null);
+
   function scrollToDiv(currentRef) {
     currentRef.current.scrollIntoView({ behavior: "smooth" });
   }
+
   return (
     <section className="homeContainer" ref={ref}>
+      <motion.div
+        animate={{ y: 0 }}
+        initial={{ y: -300 }}
+        transition={{ ease: "easeIn", duration: 1.5 }}
+        className="homeAnimateBox"
+      ></motion.div>
       <div className="homeContainer_centerBox">
         <p className="homeContainer_centerBox_p1">Hello, my name is</p>
         <div className="homeContainer_centerBox_typeWriterText">
@@ -31,14 +41,15 @@ const Home = (props, ref) => {
           </span>
           I graduated with an Associates Degree in Software Development in 2021.
           This site is a collection of the projects I've worked on, the
-          experience I've acquired, and a bit about me the developer.
+          experience I've acquired, and a bit about me the developer. Don't
+          worry about the continue button, I'll fix that.
         </p>
       </div>
-      <div className="continueBtn_container">
+      <motion.div className="continueBtn_container">
         <button className="continueBtn" onClick={() => scrollToDiv(aboutRef)}>
           Continue
         </button>
-      </div>
+      </motion.div>
     </section>
   );
 };
