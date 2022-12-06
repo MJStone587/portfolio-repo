9ef9f0bfd4c9efadfa82addabbb28f84021414f8
MJStone587 <MJstone587@yahoo.com>
2022-12-06 12:15:04 -0600

    some framer motion basic animations and potentially more

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,11 @@
 import React, { forwardRef, useRef } from "react";
 import Typewriter from "typewriter-effect";
 import "animate.css";
-import { useScroll, motion, AnimatePresence } from "framer-motion";
+import { useInView, motion, AnimatePresence } from "framer-motion";
 
 const Home = (props, ref) => {
-  const aboutRef = useRef();
-  const animRef = useRef(null);
+  const aboutRef = useRef(null);
+  const isInView = useInView(aboutRef);
 
   function scrollToDiv(currentRef) {
     currentRef.current.scrollIntoView({ behavior: "smooth" });
@@ -13,39 +13,46 @@ const Home = (props, ref) => {
 
   return (
     <section className="homeContainer" ref={ref}>
-      <motion.div
-        animate={{ y: 0 }}
-        initial={{ y: -300 }}
-        transition={{ ease: "easeIn", duration: 1.5 }}
-        className="homeAnimateBox"
-      ></motion.div>
-      <div className="homeContainer_centerBox">
-        <p className="homeContainer_centerBox_p1">Hello, my name is</p>
-        <div className="homeContainer_centerBox_typeWriterText">
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter
-                .changeDeleteSpeed(30)
-                .changeDelay(100)
-                .pauseFor(200)
-                .typeString("Mark Johnston.")
-                .start();
-            }}
-          />
-        </div>
-        <p className="homeContainer_centerBox_p2">
-          I am a...
-          <span className="homeContainer_centerBox_p2Span">
-            {" "}
-            Software Developer.{" "}
-          </span>
-          I graduated with an Associates Degree in Software Development in 2021.
-          This site is a collection of the projects I've worked on, the
-          experience I've acquired, and a bit about me the developer. Don't
-          worry about the continue button, I'll fix that.
-        </p>
+      <div className="homeAnimationRow">
+        <motion.div className="homeAnimateRogue"></motion.div>
       </div>
-      <motion.div className="continueBtn_container">
+      <AnimatePresence>
+        <motion.div
+          whileInView={{ x: 0 }}
+          initial={{ x: -1000 }}
+          className="homeContainer_centerBox"
+        >
+          <p className="homeContainer_centerBox_p1">Hello, my name is</p>
+          <div className="homeContainer_centerBox_typeWriterText">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .changeDeleteSpeed(30)
+                  .changeDelay(100)
+                  .pauseFor(200)
+                  .typeString("Mark Johnston.")
+                  .start();
+              }}
+            />
+          </div>
+          <p className="homeContainer_centerBox_p2">
+            I am a...
+            <span className="homeContainer_centerBox_p2Span">
+              {" "}
+              Software Developer.{" "}
+            </span>
+            I graduated with an Associates Degree in Software Development in
+            2021. This site is a collection of the projects I've worked on, the
+            experience I've acquired, and a bit about me the developer. Don't
+            worry about the continue button, I'll fix that.
+          </p>
+        </motion.div>
+      </AnimatePresence>
+      <motion.div
+        whileInView={{ x: 0 }}
+        initial={{ x: 800 }}
+        className="continueBtn_container"
+      >
         <button className="continueBtn" onClick={() => scrollToDiv(aboutRef)}>
           Continue
         </button>
--- src/component/ProjectDisp.js
+++ src/component/ProjectDisp.js
@@ -1,15 +1,29 @@
-import React from "react";
+import { React, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion, useInView } from "framer-motion";
 
 export default function ProjectDisp(props) {
+  const viewRef = useRef();
+  const isInView = useInView(viewRef);
+
   return (
-    <figure className="projects_carousel_window">
-      <div className="carousel_window_left">
+    <figure className="projects_carousel_window" ref={viewRef}>
+      <motion.div
+        whileInView={{ x: 0 }}
+        initial={{ x: -500 }}
+        transition={{ ease: "easeOut" }}
+        className="carousel_window_left"
+      >
         <a target="_blank" rel="noreferrer" href={props.site}>
           <img src={props.image} alt={props.alt}></img>
         </a>
-      </div>
-      <div className="carousel_window_right">
+      </motion.div>
+      <motion.div
+        whileInView={{ x: 0 }}
+        initial={{ x: 500 }}
+        transition={{ ease: "easeOut" }}
+        className="carousel_window_right"
+      >
         <div className="carousel_window_right_innerTop">
           <h1>{props.title}</h1>
           <h3>{props.description}</h3>
@@ -28,7 +42,7 @@ export default function ProjectDisp(props) {
             <FontAwesomeIcon icon="fa-solid fa-right-to-bracket" />
           </a>
         </div>
-      </div>
+      </motion.div>
     </figure>
   );
 }
