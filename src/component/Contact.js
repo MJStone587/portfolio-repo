aa85ca7ab48cd35999013070d6ea5b83e86acbcb
MJStone587 <MJstone587@yahoo.com>
2022-12-29 10:51:36 -0600

    workaround for emailJS return email address

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -37,7 +37,9 @@ const Contact = (props, ref) => {
           <input id="name_input" type="text" name="from_name" />
           <label htmlFor="email_input">Your Email:</label>
           <input id="email_input" type="email" name="from_email" />
-          <label htmlFor="message_input">Your Message:</label>
+          <label htmlFor="message_input">
+            Your Message(please include a way to contact you):
+          </label>
           <textarea id="message_input" name="message" />
           <input id="contact_submit" type="submit" value="Send" />
         </form>
