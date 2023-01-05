885cdde1b0decebf132fdcc0dc8738f0acd05db4
MJStone587 <MJstone587@yahoo.com>
2023-01-05 12:09:31 -0600

    update pokemon app addition and webp image

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -3,6 +3,7 @@ import ProjectDisp from "./ProjectDisp.js";
 import financialForgeScreen from "../images/financialForgeSS.webp";
 import what2watchScreen from "../images/what2watchScreen.webp";
 import portfolioScreen from "../images/portfolioScreen.webp";
+import pokemonScreen from "../images/pokemonAppScreen1.webp";
 
 const Projects = (props, ref) => {
   const contactRef = useRef();
@@ -40,7 +41,7 @@ const Projects = (props, ref) => {
           title="Movie/Show Browsing App"
           alt="What2Watch"
           description="Crud Project from college using PHP and mySQL. Came back to make improvements to this project, mostly styling changes."
-          tools="CSS, PHP, mySQL, Html, VScode, JS"
+          tools="CSS, PHP, mySQL, HTML, VScode, JS"
           repo="https://github.com/MJStone587/What2Watch/"
           site="https://mjstone587.github.io/What2Watch/"
         />
@@ -51,7 +52,7 @@ const Projects = (props, ref) => {
           title="Financial Organizer"
           alt="Financial Organizer"
           description="My first experience using ExpressJS / NodesJS for a CRUD app and login authentication. Understanding how to store encrypted passwords using bcrypt. Used Grid styling to better understand it."
-          tools="JS, Html, CSS, MongoDB, Express, NodeJS"
+          tools="JS, HTML, CSS, MongoDB, Express, NodeJS"
           repo="https://github.com/MJStone587/Framework"
           site="https://financialforge.onrender.com/catalog"
         />
@@ -62,9 +63,18 @@ const Projects = (props, ref) => {
           title="Portfolio"
           alt="Portfolio"
           description="My introduction to React, this project had many iterations to try out variations of styles and methods. I used this opportunity to not just learn ReactJS but to also improve my styling."
-          tools="CSS, JS, Html, ReactJS, VScode, EmailJS"
+          tools="CSS, JS, HTML, ReactJS, VScode, EmailJS"
           repo="https://github.com/MJStone587/portfolio-repo"
           site="https://markjportfolio.netlify.app/"
+        />
+        <ProjectDisp
+          image={pokemonScreen}
+          title="Pokemon Search"
+          alt="Pokemon App"
+          description="To better understand APIs, I created this Pokemon search/browse app"
+          tools="CSS, Javascript, HTML, PokeAPI, VScode  "
+          repo="https://github.com/MJStone587/Pokemon"
+          site="https://mjstone587.github.io/Pokemon/"
         />
       </div>
     </section>
