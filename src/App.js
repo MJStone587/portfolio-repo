649ddd20e6a0c54cb4fe222571032ec9aa780950
MJStone587 <MJstone587@yahoo.com>
2025-02-19 13:42:16 -0600

    fix navbar scroll flicker

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
 			}
 			oldScrollY = window.scrollY;
 		};
-		window.addEventListener("scroll", navbarControl);
 		return () => {
 			window.removeEventListener("scroll", navbarControl);
 		};
